0f2d8d0aaebc7c18773d03152d6e9b246e6f45f4
zerobig <zerobig.kim@gmail.com>
2021-01-26 12:13:51 +0900

    Update hello msg

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello~~~~ SpringBoot Docker World";
+		return "Hello~~~~ SpringBoot";
 	}
 
 	public static void main(String[] args) {
