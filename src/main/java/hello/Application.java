5689f5a9675d82394ee428183cbaf0d04addfad0
zerobig <zerobig.kim@gmail.com>
2021-01-30 15:00:11 +0900

    Update welcome msg

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Modernized SpringBoot App";
+		return "Hello Modernized SpringBoot Web App";
 	}
 
 	public static void main(String[] args) {
