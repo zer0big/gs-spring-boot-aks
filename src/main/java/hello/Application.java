e735765bb3c450e10f0045c8b24605598002b286
unknown <zerobig@sptek.co.kr>
2021-02-02 15:45:27 +0900

    Update hello msg

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello~~~~~ Modernized SB App";
+		return "Hi~~~~~ Modernized SB App";
 	}
 
 	public static void main(String[] args) {
