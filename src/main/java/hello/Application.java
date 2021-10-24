9aab596022617d4cfd5654a42a848cfeaa113302
zerobig <zerobig.kim@gmail.com>
2021-10-24 21:57:53 +0900

    Fix hello msg

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hell Modernized SB App";
+		return "Hell5 Modernized SB App";
 	}
 
 	public static void main(String[] args) {
