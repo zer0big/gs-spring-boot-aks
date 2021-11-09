c511e34f0db8efc1ea4912dee10a7f2b23df5e16
ydkim <youngdae.kim@gmail.com>
2021-11-09 13:19:37 +0900

    UPdate hello msg

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello~~~ Java App Modernization Demo";
+		return "Hellooooooo Java App Modernization Demo";
 	}
 
 	public static void main(String[] args) {
