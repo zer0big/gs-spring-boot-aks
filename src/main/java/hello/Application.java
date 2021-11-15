944771efd87de4e26a2d8068ab51e57e2e790610
zerobig <zerobig.kim@gmail.com>
2021-11-15 13:21:06 +0900

    Update hello msg

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -10,12 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
 
 	@RequestMapping("/")
-	public String home() {
-<<<<<<< HEAD
+	public String home() { 
 		return "Spring Boot Java App Modernization Demo";
-=======
-		return "Hellooooooo Java App Modernization Demo";
->>>>>>> c511e34f0db8efc1ea4912dee10a7f2b23df5e16
 	}
 
 	public static void main(String[] args) {
