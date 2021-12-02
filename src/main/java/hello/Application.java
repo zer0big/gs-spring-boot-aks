44baf5a7e07371b7c137350a9eadec42d069194d
ydkim <youngdae.kim@gmail.com>
2021-12-02 13:34:54 +0900

    Update hello msg

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -13,7 +13,7 @@ public class Application {
 
 	public String home() {
 
-		return "Hello Busan, This demo is for Modernized SB App on AKS";
+		return "Hello, This demo is for Modernized SB App on AKS";
 
 	}
 
